f9aed92f70410515a22297fa9a0045d61cb9cec3
Mizuki Morohoshi <8080suisho@gmail.com>
2020-09-29 22:43:06 +0900

    Initial Commit

---
+++ FirestoreProduct/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirestoreProduct
+//
+//  Created by 諸星水晶 on 2020/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
